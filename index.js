ecca66ed6ef242e67f474a39004ccf8df43814b1
Giacomo Mariani <giacomo.mariani@aalto.fi>
2020-12-29 11:22:19 +0200

    added root for service

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
+require('dotenv').config()
 const app = express();
-
-const port = process.env.PORT;
+const port = process.env.BE_PORT;
+const service = `${process.env.SERVICE_URL}:${process.env.SERVICE_PORT}`;
 
 app.use(cors())
+
 app.get('/', (req,res) => {
   res.end('Hello from backend')
+})
+
+app.get('/service', async (req, res) => {
+  const response = await axios.get(service);
+  res.end(response.data);
 })
 
 app.listen(port, () => {
